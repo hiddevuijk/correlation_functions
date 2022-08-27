8eb54c00f8384ce945b8f2f6e188ea4aa6d308f8
hiddevuijk <hdvuijk@gmail.com>
2022-08-27 15:01:25 +0200

    start correlation_function.h

---
+++ correlation_function.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_CORRELATION_FUNCTION_H
+#define GUARD_CORRELATION_FUNCTION_H
+
+template <class T>
+class CorrelationFunction {
+ public:
+ private:  
+};
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 
 #include "system.h"
+#include "correlation_function.h"
 
 using namespace std;
 
