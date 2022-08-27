4903234d3d3ddab6f2ed17518dc33f1f2d59745d
hiddevuijk <hdvuijk@gmail.com>
2022-08-27 20:56:42 +0200

    fix error: delta_ti is int in stead of unsigned int, because it can be negative

--- time_correlation.h
+++ time_correlation.h
@@ -22,10 +22,18 @@ class TimeCorrelation {
 
  private:  
 
-  unsigned int number_of_time_steps_;
-  unsigned int number_of_samples_;
+  void SampleCABFromList();
 
-  std::vector<T> C_AB_;
+  unsigned int number_of_time_steps_;
+  int number_of_samples_;
+
+  std::vector<T> A_list_;
+  std::vector<T> B_list_;
+
+  unsigned int list_first_index_;
+  unsigned int list_last_index_;
+
+  std::vector<T> c_AB_;
   
 };
 
@@ -33,10 +41,60 @@ class TimeCorrelation {
 template <class T>
 TimeCorrelation<T>::TimeCorrelation(unsigned int number_of_time_steps)
   : number_of_time_steps_(number_of_time_steps),
-    number_of_samples_(0),
-    C_AB_(number_of_time_steps_)
+    number_of_samples_(-1 * number_of_time_steps),
+    A_list_(number_of_time_steps),
+    B_list_(number_of_time_steps),
+    list_first_index_(0),
+    list_last_index_(-1),
+    c_AB_(number_of_time_steps_)
 {} 
 
+template <class T>
+void TimeCorrelation<T>::Sample(T A, T B)
+{
+  // if A_list_ and B_list_ not filled yet
+  if (number_of_samples_ < 0) {
+    ++list_last_index_;
+    A_list_[list_last_index_] = A;
+    B_list_[list_last_index_] = B;
+    ++number_of_samples_;
+  } else {
+    list_last_index_ = (list_last_index_ + 1) % number_of_time_steps_;
+    list_first_index_ = (list_first_index_ + 1) % number_of_time_steps_; 
+    A_list_[list_last_index_] = A; 
+    B_list_[list_last_index_] = B; 
+    SampleCABFromList();
+  } 
+}
+
+template <class T>
+void TimeCorrelation<T>::SampleCABFromList()
+{
+  ++number_of_samples_;
+
+  int delta_ti;
+  for (unsigned int i = 0; i < number_of_time_steps_; ++i) {
+    delta_ti = list_last_index_ - i;
+    if ( delta_ti < 0) {
+      delta_ti += number_of_time_steps_;
+    }
+    c_AB_[i] += (A_list_[list_last_index_] - A_list_[delta_ti]) *
+                (B_list_[list_last_index_] - B_list_[delta_ti]);
+
+  }
+  
+}
+
+template <class T>
+std::vector<T> TimeCorrelation<T>::GetTimeCorrelationFunction() const
+{
+
+  std::vector<T> c_AB_temp = c_AB_;
+  for (unsigned int i = 0; i < number_of_time_steps_; ++i) {
+    c_AB_temp[i] /= number_of_samples_;
+  }
+  return c_AB_temp;
+}
 
 
 #endif
