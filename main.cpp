7264c39786bac8966986f8a03b9b92b56fe90596
hiddevuijk <hdvuijk@gmail.com>
2022-08-27 14:50:54 +0200

    make skeleton for main

--- main.cpp
+++ main.cpp
@@ -12,6 +12,20 @@ using namespace std;
 
 int main()
 {
+  double dt = 1e-3;
+  double t_sample = 1e-2;
+  double t_total = 1e2;
+
+  long unsigned int seed = 123456789;
+
+
+  System system(seed, dt);
+ 
+  while (system.GetTime() < t_total) {
+    system.Integrate(t_sample);
+  } 
+
+
 
   return 0;
 }
